7469e7034b79a5e011caa246e569657122179d34
Chansky-Gleb <mesiagutov_gleb@mail.ru>
2022-04-25 13:35:27 +0300

    Made homework

--- Lesson5/js/main.js
+++ Lesson5/js/main.js
@@ -4,8 +4,14 @@ const app = new Vue({
     el: '#app',
     data: {
         catalogUrl: '/catalogData.json',
+        cartUrl: '/getBasket.json',
+        searchLine: '',
+        isCartVisible: false,
         products: [],
-        imgCatalog: 'https://via.placeholder.com/200x150'
+        filteredGoods: [],
+        cartItems: [],
+        imgCatalog: 'https://via.placeholder.com/200x150',
+        imgCart: 'https://via.placeholder.com/50x100',
     },
     methods: {
         getJson(url) {
@@ -15,40 +21,58 @@ const app = new Vue({
                     console.log(error);
                 })
         },
+        filterGoods() {
+            let regexp = new RegExp(this.searchLine, 'i');
+            this.filteredGoods = this.products.filter(el => regexp.test(el.product_name));
+            console.log(this.filteredGoods);
+        },
         addProduct(product) {
-            console.log(product.id_product);
-        }
+            this.getJson(`${API}/addToBasket.json`)
+                .then(data => {
+                    if (data.result === 1) {
+                        let find = this.cartItems.find(el => el.id_product === product.id_product);
+                        if (find) {
+                            find.quantity++;
+                        } else {
+                            let prod = Object.assign({
+                                quantity: 1
+                            }, product);
+                            this.cartItems.push(prod)
+                        }
+                    } else {
+                        alert('Error');
+                    }
+                })
+        },
+        deleteProduct(item) {
+            this.getJson(`${API}/deleteFromBasket.json`)
+                .then(data => {
+                    if (data.result === 1) {
+                        if (item.quantity > 1) {
+                            item.quantity--;
+                        } else {
+                            this.cartItems.splice(this.cartItems.indexOf(item), 1)
+                        }
+                    }
+                })
+
+        },
+
     },
-    beforeCreate() {
-        console.log('beforeCreate');
-        console.log(this.products);
-    },
-    created() {
-        console.log('created');
-        console.log(this.products);
+
+    mounted() {
+        this.getJson(`${API + this.cartUrl}`)
+            .then(data => {
+                for (let el of data.contents) {
+                    this.cartItems.push(el);
+                }
+            });
         this.getJson(`${API + this.catalogUrl}`)
             .then(data => {
                 for (let el of data) {
                     this.products.push(el);
+                    this.filteredGoods.push(el);
                 }
             });
-    },
-    beforeMount() {
-        console.log('beforeMount');
-    },
-    mounted() {
-        console.log('mounted');
-    },
-    beforeUpdate() {
-        console.log('beforeUpdate');
-    },
-    updated() {
-        console.log('updated');
-    },
-    beforeDestroy() {
-        console.log('beforeDestroy');
-    },
-    destroyed() {
-        console.log('destroyed');
-    },
+    }
 });
